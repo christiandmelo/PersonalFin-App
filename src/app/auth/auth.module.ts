b0af4421de28498a5536094a2b5569d1e387e2de
Christian <christiandmelo@gmail.com>
2021-10-31 13:16:19 -0300

    #6 - Front login page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +18,13 @@ import { AuthRoutingModule } from './auth-routing.module';
   imports: [
     CommonModule,
     FormsModule,
-    AuthRoutingModule
+    ReactiveFormsModule,
+    AuthRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -7,21 +11,38 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  user='';
-  password='';
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private authService: AuthService,
+    private router: Router) { }
 
-  constructor(private authService:AuthService) { }
-
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      user: ['', [
+        Validators.required
+      ]],
+      password: ['', [
+        Validators.required
+      ]]
+    });
+  }
 
   login(){
     this.authService
-        .authenticate(this.user, this.password)
+        .authenticate(
+          this.loginForm.get('user')?.value,
+          this.loginForm.get('password')?.value
+        )
         .subscribe(()=>{
-          console.log("autenticado");
+          this.router.navigate(['home/dashboard'])
         },(error) => {
-          alert("erro auh");
+          this.snackBar.open('Erro on login', 'ok', {
+            horizontalPosition: "center",
+            verticalPosition: "top",
+          });
           console.log(error);
         })
   }
