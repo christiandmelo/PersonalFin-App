c8009b4585f5e4379a7adac05d9ba9d4fa8461d5
Christian <christiandmelo@gmail.com>
2021-11-01 17:55:37 -0300

    #7 - Header in a component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HeaderModule } from '../components/header/header.module';
 
 
 @NgModule({
@@ -18,12 +13,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule
+    HeaderModule
   ],
   exports: [HomeComponent]
 })
