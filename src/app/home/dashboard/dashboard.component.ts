60e4330dc68fe2e38b06437611fba8354b9cbb90
Christian <christiandmelo@gmail.com>
2022-01-12 19:51:20 -0300

    #16 - simple html to dashboard

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  template: `
-    <p>
-      dashboard works!
-    </p>
-  `,
+  templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
