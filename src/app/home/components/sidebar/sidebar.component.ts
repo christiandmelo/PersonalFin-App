61dfcfb107778b373fa391e093d0fcea2905c3df
Christian <christiandmelo@gmail.com>
2022-01-13 07:51:55 -0300

    #7 - Enabling template icons

--- src/app/home/components/sidebar/sidebar.component.ts
+++ src/app/home/components/sidebar/sidebar.component.ts
@@ -20,7 +20,7 @@ export const ROUTES: RouteInfo[] = [
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
 
