658884442a78264b51a86f2632dedc7bbb3b9685
Christian <christiandmelo@gmail.com>
2022-01-14 08:07:12 -0300

    #7 - Enabling user menu

--- src/app/home/components/navbar/navbar.component.ts
+++ src/app/home/components/navbar/navbar.component.ts
@@ -1,36 +1,34 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import { ROUTES } from '../sidebar/sidebar.component';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/auth/user/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-  public focus !: any;
-  public listTitles !: any[];
-  public location: Location;
-  constructor(location: Location,  private element: ElementRef, private router: Router) {
-    this.location = location;
-  }
+export class NavbarComponent {
 
-  ngOnInit() {
-    this.listTitles = ROUTES.filter(listTitle => listTitle);
-  }
-  getTitle(){
-    var titlee = this.location.prepareExternalUrl(this.location.path());
-    if(titlee.charAt(0) === '#'){
-        titlee = titlee.slice( 1 );
+  user$ = this.userService.getUser();
+  showMenu = "";
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {  }
+
+  showOrHideMenu(){
+    if(this.showMenu == ""){
+      this.showMenu = "show";
+      return;
     }
 
-    for(var item = 0; item < this.listTitles.length; item++){
-        if(this.listTitles[item].path === titlee){
-            return this.listTitles[item].title;
-        }
-    }
-    return 'Dashboard';
+    this.showMenu = "";
+  }
+
+  logout(){
+    this.userService.logout();
+    this.router.navigate(['auth/login']);
   }
 
 }
