0f2611bd8e6a9bd689e488d54704f2acea8d4b70
Christian <christiandmelo@gmail.com>
2022-01-22 15:49:16 -0300

    #3 - Enabling global erros

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -4,15 +4,20 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpHeaders
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError  } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { TokenService } from './token.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private tokenService: TokenService) {}
+  constructor(
+    private tokenService: TokenService,
+    private snackBar: MatSnackBar,
+    ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
@@ -22,6 +27,44 @@ export class AuthInterceptor implements HttpInterceptor {
       request = request.clone({headers});
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        return this.handleAuthError(err);
+      })
+    )
+  }
+
+  private handleAuthError(resp: HttpErrorResponse): Observable<HttpEvent<unknown>> {
+
+    if (resp.status === 401 || resp.status === 403) {
+        this.tokenService.deleteToken();
+        return new Observable<HttpEvent<unknown>>();
+    }
+
+    try {
+        if (typeof resp.error === "string")
+        {
+          this.snackBar.open('An error occurred: ' + resp.error, 'ok', {
+            horizontalPosition: "center",
+            verticalPosition: "top",
+          });
+        }
+        else 
+        {
+          this.snackBar.open('An error occurred: ' + resp.error.data.message, 'ok', {
+            horizontalPosition: "center",
+            verticalPosition: "top",
+          });
+        }
+    } catch {
+      this.snackBar.open('An unexpected system error has occurred. Try again later.', 'ok', {
+        horizontalPosition: "center",
+        verticalPosition: "top",
+      });
+    }
+
+    console.log(resp.error);
+
+    return throwError(resp);
   }
 }
