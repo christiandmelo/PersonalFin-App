550f77e8c80b201ae43b4c6f8187e5ad347dc90e
Christian <christiandmelo@gmail.com>
2022-01-23 11:56:08 -0300

    #20 - Type menu Implementation and html graph to see incomes and expenses

--- src/app/home/app/transaction/transaction.component.ts
+++ src/app/home/app/transaction/transaction.component.ts
@@ -8,11 +8,11 @@ import { ApiResultTransactions } from './transaction';
   styleUrls: ['./transaction.component.css']
 })
 export class TransactionComponent implements OnInit {
-  page : number = 0;
-  type: number = 1;
   transactions$ !: Observable<ApiResultTransactions>;
-  classBtnType = "btn-red";
-  textBtnType = "Expenses Categories";
+  page : number = 0;
+  type: number = 1;  
+  classBtnType = "btn-purple";
+  textBtnType = "Transactions";
 
   displayedColumns: string[] = ['Status', 'Date', 'Description', 'Category', 'Account', 'Amount', 'Actions'];
 
@@ -20,5 +20,45 @@ export class TransactionComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  //#region Methods of actions on screen
+  setType(type: number){
+    this.type = type;
+
+    switch(type){
+      case 0:
+        this.classBtnType = "btn-purple";
+        this.textBtnType = "Transactions";
+        break;
+      case 1:
+        this.classBtnType = "btn-red";
+        this.textBtnType = "Expenses";
+        break;
+      case 2:
+        this.classBtnType = "btn-green";
+        this.textBtnType = "Incomes";
+        break;
+      case 3:
+        this.classBtnType = "btn-blue";
+        this.textBtnType = "Transfers";
+        break;
+      default:
+        break;
+    }
+    //this.getCategories(0);
+  }
+  //#endregion
+
+  //#region Methods of get Page
+  previousPage(){
+    this.page--;
+    //this.getCategories(this.page);
+  }
+
+  nextPage(){
+    this.page++;
+    //this.getCategories(this.page);
+  }
+  //#endregion
 
 }
